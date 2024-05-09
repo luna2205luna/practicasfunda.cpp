98f5a2b61f904a72daaa3e63b8a05360244350af
luna2205luna <164081448+luna2205luna@users.noreply.github.com>
2024-05-08 22:19:36 -0600

    Add files via upload

---
+++ practicasfunda.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=0;
+    int i=0;
+    cout <<"Ingrese un numero entero positivo" <<"\n";
+    cin >>n;
+
+    while(i<n)
+    {
+        cout << i << "\n";
+        i=i+1;
+    }
+    return 0;
+
+}
